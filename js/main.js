2e2f9c59cdbf4e236f457dd4b60aa36cf68067c7
Viktoriya Slokva <pupokxxx@mail.ru>
2018-03-05 21:01:00 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('active');
+		$('.tab-content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+	});
+
+
+   $('#myCarousel').carousel({
+           interval: 5000
+   });
+   //Handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click( function(){
+       var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+       var id = parseInt(id);
+       $('#myCarousel').carousel(id);
+       
+       $(".img-preview .thumbnail").removeClass("active");
+       $(this).toggleClass("active");
+   });
+
+});
